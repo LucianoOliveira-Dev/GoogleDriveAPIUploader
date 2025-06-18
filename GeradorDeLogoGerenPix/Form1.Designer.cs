c7fda072a074de13ecf38f9a8037f45c4c7a4a20
Usuario <Usuario@DESKTOP-LBCO7DK>
2025-06-18 19:05:58 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorDeLogoGerenPix/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace GeradorDeLogoGerenPix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelecionar = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelecionar
+            // 
+            this.btnSelecionar.Location = new System.Drawing.Point(12, 220);
+            this.btnSelecionar.Name = "btnSelecionar";
+            this.btnSelecionar.Size = new System.Drawing.Size(75, 23);
+            this.btnSelecionar.TabIndex = 0;
+            this.btnSelecionar.Text = "Selecionar";
+            this.btnSelecionar.UseVisualStyleBackColor = true;
+            this.btnSelecionar.Click += new System.EventHandler(this.btnSelecionar_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(134, 220);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 1;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // picLogo
+            // 
+            this.picLogo.Location = new System.Drawing.Point(12, 12);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(197, 157);
+            this.picLogo.TabIndex = 2;
+            this.picLogo.TabStop = false;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 290);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(100, 13);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Aguardando upload";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 257);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(197, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Location = new System.Drawing.Point(12, 186);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(197, 20);
+            this.txtDocumento.TabIndex = 5;
+            this.txtDocumento.Text = "Digite o CNPJ aqui";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 311);
+            this.Controls.Add(this.txtDocumento);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.picLogo);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnSelecionar);
+            this.Name = "Form1";
+            this.Text = "Carregar imagem para Drive";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelecionar;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txtDocumento;
+    }
+}
+
---
+++ GeradorDeLogoGerenPix/Form1.cs
@@ -0,0 +1,222 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace GeradorDeLogoGerenPix
+{
+    public partial class Form1 : Form
+    {
+
+        private string      _filePath = string.Empty;
+        private long        _currentUploadTotalBytes;
+        private const int   MAX_SIZE_MB = 1; // Aqui determina o tamanho maximo da imagem
+
+        private string PARENT_FOLDER_ID => AppConfig.ParentFolderId;
+
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 0;
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSelecionar_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Imagens|*.png;*.jpg;*.jpeg";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    _filePath = openFileDialog.FileName;
+                    FileInfo fileInfo = new FileInfo(_filePath);
+
+                    if (fileInfo.Length > MAX_SIZE_MB * 1024 * 1024)
+                    {
+                        lblStatus.Text = "Erro: Arquivo maior que 1MB!";
+                        btnUpload.Enabled = false;
+                        picLogo.Image = null;
+                        return;
+                    }
+
+                    picLogo.ImageLocation = _filePath;
+                    lblStatus.Text = "Pronto para upload!";
+                    btnUpload.Enabled = true;
+                }
+            }
+        }
+
+        private async void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(_filePath))
+            {
+                MessageBox.Show("Selecione um arquivo primeiro!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtDocumento.Text))
+            {
+                MessageBox.Show("Informe o CNPJ/CPF do cliente!");
+                return;
+            }
+
+            btnUpload.Enabled = false;
+            progressBar1.Value = 0;
+            lblStatus.Text = "Autenticando...";
+
+            try
+            {
+                _currentUploadTotalBytes = new FileInfo(_filePath).Length;
+
+                UserCredential credential;
+                using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+                {
+                    credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        new[] { DriveService.Scope.DriveFile },
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore("token.json", true));
+                }
+
+                var service = new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Upload Logo Drive",
+                });
+
+                // Verifica se a pasta com o CNPJ já existe ou cria uma nova
+                string cnpjFolderId = await GetOrCreateFolder(service, txtDocumento.Text);
+
+                lblStatus.Text = "Enviando para o Google Drive...";
+                progressBar1.Style = ProgressBarStyle.Blocks;
+
+                var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+                {
+                    Name = Path.GetFileName(_filePath),
+                    Parents = new[] { cnpjFolderId } // Usa a pasta do CNPJ como parent
+                };
+
+                using (var stream = new FileStream(_filePath, FileMode.Open))
+                {
+                    var request = service.Files.Create(fileMetadata, stream, "image/jpeg");
+                    request.Fields = "id, webViewLink";
+                    request.ProgressChanged += Upload_ProgressChanged;
+
+                    var progress = await request.UploadAsync();
+
+                    if (progress.Status == UploadStatus.Completed)
+                    {
+                        var file = request.ResponseBody;
+                        lblStatus.Text = "Upload concluído!";
+                        progressBar1.Value = 100;
+                        MessageBox.Show($"Logo salvo em: {file.WebViewLink}", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        lblStatus.Text = "Erro: " + progress.Exception.Message;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Erro: " + ex.Message;
+                MessageBox.Show($"Falha na autenticação ou upload. Detalhes: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                btnUpload.Enabled = true;
+            }
+        }
+
+        private async Task<string> GetOrCreateFolder(DriveService service, string folderName)
+        {
+
+            if (string.IsNullOrEmpty(PARENT_FOLDER_ID))
+            {
+                throw new InvalidOperationException("ParentFolderId não configurado no appsettings.json");
+            }  // Verifica se o appSettings esta configurado ********************************
+
+            // verifica se a pasta já existe
+            var listRequest = service.Files.List();
+            listRequest.Q = $"mimeType='application/vnd.google-apps.folder' and name='{folderName}' and '{PARENT_FOLDER_ID}' in parents and trashed=false";
+            listRequest.Fields = "files(id, name)";
+
+            var folders = await listRequest.ExecuteAsync();
+
+            if (folders.Files.Count > 0)
+            {
+                return folders.Files[0].Id; // Retorna o ID se a pasta existir xD
+            }
+
+            // Se não existe, cria uma nova :o
+            var folderMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = folderName,
+                MimeType = "application/vnd.google-apps.folder",
+                Parents = new[] { PARENT_FOLDER_ID }
+            };
+
+            var request = service.Files.Create(folderMetadata);
+            request.Fields = "id";
+            var folder = await request.ExecuteAsync();
+
+            return folder.Id;
+        }
+
+        private void Upload_ProgressChanged(IUploadProgress progress)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                switch (progress.Status)
+                {
+                    case UploadStatus.Uploading:
+                        int percentage = (int)(progress.BytesSent * 100 / _currentUploadTotalBytes);
+                        progressBar1.Value = Math.Min(percentage, 100);
+                        lblStatus.Text = $"Enviando... {percentage}%";
+                        break;
+
+                    case UploadStatus.Completed:
+                        progressBar1.Value = 100;
+                        lblStatus.Text = "Upload concluído!";
+                        break;
+
+                    case UploadStatus.Failed:
+                        progressBar1.Value = 0;
+                        lblStatus.Text = "Falha no upload";
+                        break;
+                }
+            });
+        }
+
+        public static class AppConfig
+        {
+            public static IConfigurationRoot Configuration { get; private set; }
+
+            static AppConfig()
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true);
+
+                Configuration = builder.Build();
+            }
+
+            public static string ParentFolderId => Configuration["DriveSettings:ParentFolderId"];
+        }
+    }
+}
